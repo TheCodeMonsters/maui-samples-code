7196d0f8ff8ad9d8b7eabd4c7ae04608c3f2cd0b
Manuel Duarte <carlosd99m@gmail.com>
2021-06-27 12:24:47 -0600

    add poject Flyout

---
+++ Flyout/Flyout/Flyout/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Flyout.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Flyout.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
