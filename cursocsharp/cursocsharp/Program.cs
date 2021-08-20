7f4954b5ac720890239cdf9ff408867e1183245b
Manuel Duarte <carlosd99m@gmail.com>
2021-08-20 00:41:17 -0600

    update

--- cursocsharp/cursocsharp/Program.cs
+++ cursocsharp/cursocsharp/Program.cs
@@ -19,6 +19,7 @@ namespace cursocsharp
             short? edad = 20;
             Int16 altura = 178;
             float alturaEnMetros = 1.78f;
+            string position = "Xamarin Developer";
 
             Console.WriteLine("Hola " + name + lastName);
 
@@ -34,11 +35,32 @@ namespace cursocsharp
             Console.WriteLine($"Mi apellido tiene {lastName.Trim().Replace(" ", "").Length} letras");
             // Para reamplacer cualquier carater por otro .Remplae()
             Console.WriteLine($"Mi edad es: {edad}"); // Esta es la menera mas moderna de concatenar los string en c#
-          
+
 
             Console.WriteLine($"Mi altura es: {altura} cms");
 
             Console.WriteLine($"Mi altura en metros es: {alturaEnMetros}");
+
+            Console.WriteLine($"Con ustedes {name} {lastName} con la edad de {edad} y es un {position}");
+
+
+            #region fechas
+
+            DateTime fecha = new DateTime(2001, 6, 1);
+            var fechaActual = DateTime.Now;
+
+
+            // 
+            Console.WriteLine($"Mi fecha de nacimiento es: {fecha.Date.ToString("MM/dd/yyyy")}");
+
+            Console.WriteLine($"La fecha de hoy es {fechaActual}");
+            Console.WriteLine($"La fecha de hoy es {fechaActual.Date.ToString("MM/dd/yy")}");
+
+            var diferentesFechas = fechaActual - fecha;
+
+            Console.WriteLine($"Mi edad es {diferentesFechas.Days / 365}");
+
+            #endregion fechas            
 
         }
     }
